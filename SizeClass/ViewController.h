ae6f71f0470b4c13d0abdc837f7be8ba9bfed76c
xunyn <hsbc_xian_core@HSBC-XiAn-CoredeMacBook-Pro.local>
2015-08-25 16:23:17 +0800

    initial
    initial

---
+++ SizeClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SizeClass
+//
+//  Created by HSBC_XiAn_Core on 8/25/15.
+//  Copyright (c) 2015 HSBC_XiAn_Core. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SizeClass/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SizeClass
+//
+//  Created by HSBC_XiAn_Core on 8/25/15.
+//  Copyright (c) 2015 HSBC_XiAn_Core. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
